86051f54bc9c12736c5ef57d2d96b0713193f3b5
fireblaster9967asad <adrshazia@gmail.com>
2026-02-24 04:46:26 -0800

    Add files via upload

---
+++ calander.js
@@ -0,0 +1,9 @@
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const today = weekdays[date.getDay()];
+
+const calander = "Date: " + day + "/" + month + "/" + year + " Day: " + today;
+document.getElementById("Mycalander").innerText = calander;
