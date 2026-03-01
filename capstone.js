e1abe72f90f3ca8ec66587195cfa64974026a419
fireblaster9967asad <adrshazia@gmail.com>
2026-03-01 00:13:38 -0800

    Add files via upload

---
+++ capstone.js
@@ -0,0 +1,14 @@
+var myindex = 0;
+
+function carousel() {
+    var i;
+
+    var x = document.getElementsByClassName("myslides")
+    for (i = 0; < x.length; i++) {
+        x[i].
+    }
+
+
+}
+
+carousel();
